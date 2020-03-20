2bdf74af661dc7eeebeef02f192063660796627c
jwilkinson6179 <JamesW6179@gmail.com>
2020-03-20 18:46:54 -0400

    Fundies are done, going to do lambdas for them later tho

--- src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
@@ -5,31 +5,48 @@ package rocks.zipcode.quiz4.fundamentals;
  */
 public class Calculator {
     public static Double squareRoot(Double value) {
-        return null;
+        return Math.sqrt(value);
     }
 
-    public static Double square(Double value) {
-        return null;
+    public static Double square(Double value)
+    {
+        return Math.pow(value, 2);
     }
 
-    public static Double[] squareRoots(Double... value) {
-        return null;
+    public static Double[] squareRoots(Double... value)
+    {
+        Double[] results = new Double[value.length];
+
+        for(Integer i = 0; i < value.length; i++)
+        {
+            results[i] = squareRoot(value[i]);
+        }
+
+        return results;
     }
 
-    public static Double[] squares(Double... values) {
-        return null;
+    public static Double[] squares(Double... values)
+    {
+        Double[] results = new Double[values.length];
+
+        for(Integer i = 0; i < values.length; i++)
+        {
+            results[i] = square(values[i]);
+        }
+
+        return results;
     }
 
     public static Double add(Double value1, Double value2) {
-        return null;
+        return value1 + value2;
     }
 
     public static Double subtract(Double value1, Double value2) {
-        return null;
+        return value1 - value2;
     }
 
 
     public static Double divide(Double divisor, Double dividend) {
-        return null;
+        return divisor / dividend;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -3,32 +3,123 @@ package rocks.zipcode.quiz4.fundamentals;
 /**
  * @author leon on 21/12/2018.
  */
-public class StringUtils {
-    public static Character getMiddleCharacter(String string) {
-        return null;
+public class StringUtils
+{
+    public static Character getMiddleCharacter(String string)
+    {
+        return string.charAt(string.length() / 2);
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        Integer charIdx = str.length() / 2;
+
+        for(Integer i = 0; i < str.length(); i++)
+        {
+            if(i == charIdx)
+            {
+                result.append(Character.toUpperCase(str.charAt(i)));
+            }
+            else
+            {
+
+                result.append(str.charAt(i));
+            }
+        }
+        return result.toString();
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        Integer charIdx = str.length() / 2;
+
+        for(Integer i = 0; i < str.length(); i++)
+        {
+            if(i == charIdx)
+            {
+                result.append(Character.toLowerCase(str.charAt(i)));
+            }
+            else
+            {
+
+                result.append(str.charAt(i));
+            }
+        }
+        return result.toString();
     }
 
-    public static Boolean isIsogram(String str) {
-        return null;
+    public static Boolean isIsogram(String str)
+    {
+        for(Integer i = 0; i < str.length(); i++)
+        {
+            for(Integer k = i + 1; k < str.length(); k++)
+            {
+                if(str.charAt(i) == (str.charAt(k)))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
-    public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+    public static Boolean hasDuplicateConsecutiveCharacters(String str)
+    {
+        for(Integer i = 1; i < str.length(); i++)
+        {
+            if(str.charAt(i - 1) == str.charAt(i))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    public static String removeConsecutiveDuplicateCharacters(String str) {
-        return null;
+    public static String removeConsecutiveDuplicateCharacters(String str)
+    {
+        StringBuilder result = new StringBuilder();
+
+        Character current = str.charAt(0);
+        if(!current.equals(str.charAt(1)))
+        {
+            result.append(current);
+        }
+
+        for(Integer i = 1; i < str.length() - 1; i++)
+        {
+            current = str.charAt(i);
+            if(!current.equals(str.charAt(i - 1)) && !current.equals(str.charAt(i + 1)))
+            {
+                result.append(current);
+            }
+        }
+
+        current = str.charAt(str.length() - 1);
+        if(!current.equals(str.charAt(str.length() - 2)))
+        {
+            result.append(current);
+        }
+
+        return result.toString();
     }
 
-    public static String invertCasing(String str) {
-        return null;
+    public static String invertCasing(String str)
+    {
+        StringBuilder result = new StringBuilder();
+
+        for(Integer i = 0; i < str.length(); i++)
+        {
+            if(Character.isUpperCase(str.charAt(i)))
+            {
+                result.append(Character.toLowerCase(str.charAt(i)));
+            }
+            else
+            {
+                result.append(Character.toUpperCase(str.charAt(i)));
+            }
+        }
+
+        return result.toString();
     }
 }
